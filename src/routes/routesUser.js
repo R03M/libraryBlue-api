3be21b55c89e77dcfeb25e29ec9fed417c3d0178
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-03 20:37:15 -0300

    Update

--- src/routes/routesUser.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getUser, postUsers } from "../controllers/users.js";
-
-const router = Router();
-
-router.get("/", getUser);
-router.post("/", postUsers);
-
-export default router;
