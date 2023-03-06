8bbc09cb1afe1744de5c401699b19f89e06b2482
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-06 16:51:29 -0300

    Add validation

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,9 +1,23 @@
-export const errorRegister = (email, password, isGoogle) => {
+export const errorRegister = (
+  email,
+  password,
+  isGoogle,
+  firstName,
+  lastName,
+  image,
+  position,
+  status
+) => {
   const errors = [];
- 
+
   if (!email) errors.push("Email is required");
   if (!password) errors.push("Password is required");
   if (isGoogle === undefined) errors.push("isGoogle is required");
+  if (!firstName) errors.push("First Name is required");
+  if (!lastName) errors.push("Last Name is required");
+  if (!image) errors.push("Image is required");
+  if (!position) errors.push("Position is required");
+  if (!status) errors.push("Status is required");
 
   if (errors.length > 0) {
     return { errorMessage: errors.join(", ") };
