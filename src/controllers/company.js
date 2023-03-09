5c308bbf8896b2cbddd48115266919e85f4acc5d
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-08 21:19:10 -0300

    Add Update company and minimal fix at register

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -18,20 +18,19 @@ export const registerCompany = async (req, res) => {
     const existingCompanies = allCompanies.map((e) => e.name);
 
     if (existingCompanies.includes(name)) {
-      res
+      return res
         .status(406)
         .json({ message: "There is already a company with that name" });
-    } else {
-      const newCompany = await CompanyModel.create({
-        name,
-        image,
-        associatedCompany,
-      });
-
-      const response = await newCompany.addUser(idUser);
-
-      res.status(200).json({ message: "Company Created", response });
     }
+    const newCompany = await CompanyModel.create({
+      name,
+      image,
+      associatedCompany,
+    });
+
+    const response = await newCompany.addUser(idUser);
+
+    res.status(200).json({ message: "Company Created", response });
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
   }
@@ -48,6 +47,25 @@ export const deleteCompany = async (req, res) => {
     companyDelete !== 0
       ? res.status(200).json({ message: "Company successfully removed" })
       : res.status(404).json({ message: "Company not found" });
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
+
+export const updateCompany = async (req, res) => {
+  const { id, name, image, associatedCompany } = req.body;
+
+  try {
+    const updated = await CompanyModel.update(
+      {
+        name,
+        image,
+        associatedCompany,
+      },
+      { where: { id: id } }
+    );
+
+    res.status(200).json({ message: "Update successful", updated });
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
   }
