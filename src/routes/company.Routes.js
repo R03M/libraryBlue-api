ecef8d4080bd1c6453a1d5d80175b12440a43908
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-03-08 21:20:16 -0300

    Add route /update

--- src/routes/company.Routes.js
+++ src/routes/company.Routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getCompanies, registerCompany, deleteCompany } from "../controllers/company.js";
+import { getCompanies, registerCompany, deleteCompany, updateCompany } from "../controllers/company.js";
 
 const router = Router();
 
 router.get("/all", getCompanies);
 router.post("/new", registerCompany);
 router.delete("/delete", deleteCompany);
+router.post("/update", updateCompany);
 
 export default router;
