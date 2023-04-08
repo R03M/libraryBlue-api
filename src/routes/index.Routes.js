b5801d28c393dd0dbe9fac873aa10b1b9e2f3362
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-07 23:03:28 -0300

    Add: authMiddleware

--- src/routes/index.Routes.js
+++ src/routes/index.Routes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import userRoutes from "./user.Routes.js";
 import registerRoutes from "./register.Routes.js";
-import companiesRoutes from "./company.Routes.js"
-import itemsRoutes from "./item.Routes.js"
-import loginRoutes from "./login.Routes.js"
+import companiesRoutes from "./company.Routes.js";
+import itemsRoutes from "./item.Routes.js";
+import loginRoutes from "./login.Routes.js";
+import authMiddleware from "../utils/authMiddleware.js";
 
 const router = Router();
 
-router.use("/user", userRoutes);
-router.use("/register", registerRoutes);
-router.use("/company", companiesRoutes);
-router.use("/item", itemsRoutes);
 router.use("/login", loginRoutes);
-
+router.use("/register", registerRoutes);
+router.use("/user", authMiddleware, userRoutes);
+router.use("/company", authMiddleware, companiesRoutes);
+router.use("/item", authMiddleware, itemsRoutes);
 
 export default router;
