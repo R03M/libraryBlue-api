d49e3c490eea7b9cb26a945aaa59fc64785c4c0e
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-04-14 00:58:47 -0300

    Update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", CORS_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
